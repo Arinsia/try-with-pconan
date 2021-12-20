caec3f009c8b01d9ddaded085015576ae214671c
Arinsia <95367448+Arinsia@users.noreply.github.com>
2021-12-20 08:13:45 +0700

    Update main.py
    เอา#ออก

--- main.py
+++ main.py
@@ -43,41 +43,41 @@ def total(list2):
     return summation
 
 #ส่งงาน 2 
-#@app.get("/how_to_find_pythagorean")
-#from math import sqrt
+@app.get("/how_to_find_pythagorean")
+from math import sqrt
 
-#print('Assume the sides are a, b, c and c is the hypotenuse (the side opposite the right angle')
-#formula = input('Which side (a, b, c) do you wish to calculate? side> ')
+print('Assume the sides are a, b, c and c is the hypotenuse (the side opposite the right angle')
+formula = input('Which side (a, b, c) do you wish to calculate? side> ')
 
-#if formula == 'c':
-	#side_a = int(input('Input the length of side a: '))
-	#side_b = int(input('Input the length of side b: '))
+if formula == 'c':
+	side_a = int(input('Input the length of side a: '))
+	side_b = int(input('Input the length of side b: '))
 
-	#side_c = sqrt(side_a * side_a + side_b * side_b)
+	side_c = sqrt(side_a * side_a + side_b * side_b)
 	
-	#print('The length of side c is: ' )
-	#print(side_c)
+	print('The length of side c is: ' )
+	print(side_c)
 
-#elif formula == 'a':
-    #side_b = int(input('Input the length of side b: '))
-    #side_c = int(input('Input the length of side c: '))
+elif formula == 'a':
+    side_b = int(input('Input the length of side b: '))
+    side_c = int(input('Input the length of side c: '))
     
-    #side_a = sqrt((side_c * side_c) - (side_b * side_b))
+    side_a = sqrt((side_c * side_c) - (side_b * side_b))
     
-    #print('The length of side a is' )
-    #print(side_a)
+    print('The length of side a is' )
+    print(side_a)
 
-#elif formula == 'b':
-    #side_a = int(input('Input the length of side a: '))
-    #side_b = int(input('Input the length of side c: '))
+elif formula == 'b':
+    side_a = int(input('Input the length of side a: '))
+    side_b = int(input('Input the length of side c: '))
         
-    #side_c = sqrt(side_c * side_c - side_a * side_a)
+    side_c = sqrt(side_c * side_c - side_a * side_a)
     
-    #print('The length of side b is')
+    print('The length of side b is')
     #print(side_c)
 
-#else:
-	#print('Please select a side between a, b, c')#/
+else:
+	print('Please select a side between a, b, c')
 
 @app.get("/add")
 async def add(a: int = 0, b: int = 0):
