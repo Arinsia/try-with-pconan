476c4b426573c4be3ff0b6814ac4635ee71ca86a
nun <tsuyuyu1@gmail.com>
2021-12-20 23:39:16 +0700

    รัน พีทาได้ หาพท.วงกลมใน fast

--- main.py
+++ main.py
@@ -38,6 +38,38 @@ async def add(a: int = 2343240987, b: int = 20034932):
 async def circle_area(radius: float):
     return math.pi * radius ** 2
 
+#ไม่ได้ใช้
+from math import sqrt
+app.get("/how_to_find_pythagorean")
+
+print('Assume the sides are a, b, c and c is the hypotenuse (the side opposite the right angle')
+formula = input('Which side (a, b, c) do you wish to calculate? side> ')
+if formula == 'c':
+	side_a = int(input('Input the length of side a: '))
+	side_b = int(input('Input the length of side b: '))
+	side_c = sqrt(side_a * side_a + side_b * side_b)
+	
+	print('The length of side c is: ' )
+	print(side_c)
+elif formula == 'a':
+    side_b = int(input('Input the length of side b: '))
+    side_c = int(input('Input the length of side c: '))
+    
+    side_a = sqrt((side_c * side_c) - (side_b * side_b))
+    
+    print('The length of side a is' )
+    print(side_a)
+elif formula == 'b':
+    side_a = int(input('Input the length of side a: '))
+    side_b = int(input('Input the length of side c: '))
+        
+    side_c = sqrt(side_c * side_c - side_a * side_a)
+    
+    print('The length of side b is')
+    print(side_c)
+else:
+	print('Please select a side between a, b, c')
+
 
 @app.get("/add")
 async def add(a: int = 0, b: int = 0):
